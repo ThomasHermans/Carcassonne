9d0715c2246d0f8c0091571465e2fbd90cb7c0b0
Thomas Hermans <thomashhermans+github@gmail.com>
2014-03-02 14:29:18 +0100

    More Board unit tests

--- Model/Private/UnitTests/BoardUnitTests.cpp
+++ Model/Private/UnitTests/BoardUnitTests.cpp
@@ -66,6 +66,41 @@ TEST( "Board: check for (im)possible tiles" )
 	board.placeStartTile( TileOnBoard( createTileD(), kCw0 ) );
 	board.placeValidTile( TileOnBoard( createTileE(), kCw270 ), 4, 3 );
 
+	REQUIRE( board.getNrOfTiles() == 2 );
 	CHECK( board.isPossibleTile( createTileA() ) );
 	CHECK( !board.isPossibleTile( createTileC() ) );
+}
+
+TEST( "Board: check for empty spots" )
+{
+	Board board( 5 );
+	board.placeStartTile( TileOnBoard( createTileD(), kCw0 ) );
+	board.placeValidTile( TileOnBoard( createTileE(), kCw270 ), 3, 2 );
+
+	REQUIRE( board.getNrOfTiles() == 2 );
+	CHECK( board.isEmptySpot( 1, 1 ) );
+	CHECK( board.isEmptySpot( 2, 1 ) );
+	CHECK( board.isEmptySpot( 3, 1 ) );
+	CHECK( board.isEmptySpot( 1, 2 ) );
+	CHECK( !board.isEmptySpot( 2, 2 ) );
+	CHECK( !board.isEmptySpot( 3, 2 ) );
+	CHECK( board.isEmptySpot( 1, 3 ) );
+	CHECK( board.isEmptySpot( 2, 3 ) );
+	CHECK( board.isEmptySpot( 3, 3 ) );
+}
+
+TEST( "Board: check for finished cities" )
+{
+	Board board( 7 );
+	board.placeStartTile( TileOnBoard( createTileD(), kCw0 ) );
+	board.placeValidTile( TileOnBoard( createTileN(), kCw270 ), 4, 3 );
+	board.placeValidTile( TileOnBoard( createTileN(), kCw0 ), 4, 4 );
+
+	REQUIRE( board.getNrOfTiles() == 3 );
+	CHECK( !board.isFinishedCity( 3, 3, Area::kRight ) );
+
+	board.placeValidTile( TileOnBoard( createTileK(), kCw0 ), 3, 4 );
+
+	REQUIRE( board.getNrOfTiles() == 4 );
+	CHECK( board.isFinishedCity( 3, 3, Area::kRight ) );
 }
