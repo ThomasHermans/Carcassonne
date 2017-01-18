2fbcfb3186fd77e2d4c57867fc29f8d27a624f06
ThomasHermans <thhr@eaw14ml171.local>
2017-01-18 16:29:19 -0600

    More BoardUtils unit tests

--- Model/Private/UnitTests/BoardUtilsUnitTests.cpp
+++ Model/Private/UnitTests/BoardUtilsUnitTests.cpp
@@ -8,6 +8,8 @@
 
 #include "Utils/Typedefs.h"
 
+#include <utility>
+
 namespace
 {
 	struct BoardFixture
@@ -65,4 +67,38 @@ TESTFIX( "BoardUtils: getTilesToFillCloister returns the correct result.", Board
 	{
 		CHECK( std::find( missingTiles.cbegin(), missingTiles.cend(), location ) != missingTiles.cend() );
 	}
+}
+
+TESTFIX( "BoardUtils: isFillable returns the correct result.", BoardFixture )
+{
+	std::vector< Model::Tile > cloistersAndRoads = { Model::createTileB(), Model::createTileV(), Model::createTileA() };
+	CHECK( !isFillable( board, { -1, 1 }, cloistersAndRoads ) );
+	CHECK( isFillable( board, { 1, 0 }, cloistersAndRoads ) );
+	std::vector< Model::Tile > tiles = { Model::createTileA(), Model::createTileC(), Model::createTileI() };
+	CHECK( isFillable( board, { -1, 1 }, tiles ) );
+	CHECK( isFillable( board, { 1, 0 }, tiles ) );
+}
+
+TESTFIX( "BoardUtils: getPieces returns a list of all the pieces of a color on the board.", BoardFixture )
+{
+	Model::PlacedPiece const redPieceOne{ Model::Piece{ Model::Piece::kFollower, Model::Color::kRed }, Model::Area::kLeft };
+	Utils::Location const locationRPO{ 0, 1 };
+	board.placeValidPiece( redPieceOne, locationRPO );
+	board.placeValidTile( Model::TileOnBoard{ Model::createTileV(), Model::kCw90 }, { 1, 0 } );
+	Model::PlacedPiece const redPieceTwo{ Model::Piece{ Model::Piece::kFollower, Model::Color::kRed }, Model::Area::kTop };
+	Utils::Location const locationRPT{ 1, 0 };
+	board.placeValidPiece( redPieceTwo, locationRPT );
+	board.placeValidTile( Model::TileOnBoard{ Model::createTileB(), Model::kCw0 }, { 1, 1 } );
+	Model::PlacedPiece const greenPieceOne{ Model::Piece{ Model::Piece::kFollower, Model::Color::kGreen }, Model::Area::kCentral };
+	Utils::Location const locationGPO{ 1, 1 };
+	board.placeValidPiece( greenPieceOne, locationGPO );
+
+	auto const redPieces = getPieces( board, Model::Color::kRed );
+	CHECK( redPieces.size() == 2 );
+	CHECK( std::find( redPieces.cbegin(), redPieces.cend(), std::make_pair( locationRPO, redPieceOne ) ) != redPieces.cend() );
+	CHECK( std::find( redPieces.cbegin(), redPieces.cend(), std::make_pair( locationRPT, redPieceTwo ) ) != redPieces.cend() );
+
+	auto const greenPieces = getPieces( board, Model::Color::kGreen );
+	CHECK( greenPieces.size() == 1 );
+	CHECK( std::find( greenPieces.cbegin(), greenPieces.cend(), std::make_pair( locationGPO, greenPieceOne ) ) != greenPieces.cend() );
 }
